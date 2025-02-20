73b0c30f33947e6549310d18221f6d37a4c6f15e
hogyzen12 <billypapas12@gmail.com>
2025-02-20 19:02:35 +0100

    pushing new vault setup

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -5,7 +5,7 @@ export const TOKEN_X_MINT = new PublicKey("GovJupiFjKj6dY44qHT4i2FzDejKTCZSvKpab
 export const TOKEN_Y_MINT = new PublicKey("JUPyiwrYJFskUPiHa7hkeR8VUtAeFoSYbKedZNsDvCN");
 
 export const STAKED_JUP = new PublicKey("CwB36s8KFNLoCu99wLeM1BMLMMRZUthNX5wH8JNo43sZ");
-export const VAULT_JUP = new PublicKey("BDunM82nxZJB6Yj8RAY98kvxqFvTqVwefMHShTNkeYfh");
+export const VAULT_JUP = new PublicKey("5tDex4LSPm8fnz54w586S73tQnNA1h2KRLWRm7Vmw3Nu");
 
 export const GOV_TOKEN = new PublicKey('GovJupiFjKj6dY44qHT4i2FzDejKTCZSvKpabZYQSFF');
 export const AGENT_ADDRESS = new PublicKey('govAiNGS59yJfvqdmvYb7Lzk7wUHYygqiKR58Z1VCag');
